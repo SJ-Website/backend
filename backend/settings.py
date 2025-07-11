e491a8cf23adb5b698169c4593b84e106de3508e
Kisuke <kisuke@kali.kali>
2025-07-12 02:09:23 +0530

    error correction

--- backend/settings.py
+++ backend/settings.py
@@ -32,8 +32,15 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACHEND = 'django.core.mail.backends.smtp.EmailBackend'
-ALLOWED_HOSTS = []
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "https://backend-svqg.onrender.com",
+    "backend-svqg.onrender.com",
+    "sjfrontend.vercel.app",
+]
+
 
 
 # Application definition
