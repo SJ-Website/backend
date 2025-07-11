f8b652d597b7cfe0cad8f543ce98c13651240083
Kisuke <kisuke@kali.kali>
2025-07-12 01:10:14 +0530

    error correction

--- backend/settings.py
+++ backend/settings.py
@@ -154,6 +154,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.1:3000",
     "http://localhost:5173",
     "http://127.0.1:5173",
+    "https://frontend-1qlpbqlh8-anita-maxwynns-projects.vercel.app/"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
